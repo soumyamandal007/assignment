83e3f2659775422694e9b488c5aba897bf0c2398
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2024-07-18 12:31:50 -0700

    Update settings.py

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!n1d66l4jwyj)!1(qq+g4x8)_%&$e&f6p039zgn)-!5wj87s*a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
