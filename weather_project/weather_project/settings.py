9942bbe666d0f4224cc9ea8085f00ef6a0cf7aad
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2024-07-18 12:40:54 -0700

    Update settings.py

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!n1d66l4jwyj)!1(qq+g4x8)_%&$e&f6p039zgn)-!5wj87s*a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
